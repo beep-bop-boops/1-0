b6a62c4118c352499bbcf10cbd1547496450ec81
huseyin1596498 <huseyinucur@yahoo.co.uk>
2019-03-16 23:42:18 +0000

    inf.cpp update

--- inf.cpp
+++ inf.cpp
@@ -6,61 +6,33 @@
 using namespace std;
 int main()
 {
-
  int zero, speed;
  speed = 200;
 
-
 for(long i=1; i<=1000000000; i++)
 {
-    /*
-    for(int o=1; o<=8; o++)
+
+if(GetKeyState('W') & 0x8000
     {
-          cout << o;
-          if(o = 1){cout << "" ;}
-          else if(o = 2){cout << "hi 2";}
-          else{cout << "rest";}
-    else
-        {
-         cout << "1";
-        }
-
-    } 
-    cout << "in //";
+        speed = speed - 5;
     }
-    
-    
-    cout << "out // ";
-/test update
-    */
 
-   if(GetKeyState('W') & 0x8000/*Check if high-order bit is set (1 << 15)*/)
-{
-    
-    speed = speed - 5;
-   
-    
-}
-   if(GetKeyState('Q') & 0x8000/*Check if high-order bit is set (1 << 15)*/)
-{
-    speed = speed + 5;
-    
-}
+ if(GetKeyState('Q') & 0x8000
+    {
+        speed = speed + 5;
+    }
 
 zero = (rand() %2 + 1) - 1;
- if(speed <= 0)
+ 
+if(speed <= 0)
     {
         speed = 5;
     }
 
 cout << zero;
-/* debug
-cout << endl;
-cout << speed;
-cout << endl;
-*/
+
 Sleep(speed);
 }
-return 0;
 
+return 0;
 }
